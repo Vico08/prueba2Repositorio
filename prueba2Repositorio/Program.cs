8f5f086b5dfd0586ed05977c336aa1c2991c7e89
Victoria Cano <victoriacano@outlook.es>
2024-09-13 19:28:04 -0500

    prueba 1 ejercicio triangulo

--- prueba2Repositorio/Program.cs
+++ prueba2Repositorio/Program.cs
@@ -1,7 +1,42 @@
-﻿internal class Program
+﻿public class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("PRUEBA 2 DE CREACION DE REPOSITORIO!");
+
+        double seg1, seg2, seg3, suma;
+        bool isTriangle;
+        string msj;
+
+        Console.Write("INgrese el primer segmento: ");
+        seg1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("INgrese el segundo segmento: ");
+        seg2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("INgrese el tercer segmento: ");
+        seg3 = Convert.ToDouble(Console.ReadLine());
+
+        if ((seg1 + seg2) > seg3)
+        {
+            if ((seg1 + seg3) > seg2)
+            {
+                if ((seg1 + seg3) > seg1)
+                {
+                    isTriangule = true;
+                }
+                else
+                {
+                    isTriangule = false;
+                }
+            }
+            else
+            {
+                isTriangule = false;
+            }
+        }
+        else
+        {
+            isTriangule = false;
+        }
     }
 }
